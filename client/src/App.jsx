829fed3b840e29e2c483ef1a0bd616222c491e14
3MaheshKadam <maheshkadam9298@gmail.com>
2024-06-16 16:46:54 +0530

    display 3 (coth , cosmetic , electric ) categorial products based on single category function

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,15 +5,19 @@ import Footer from './components/Footer.jsx';
 import Home from './Pages/Home.jsx';
 import Category from './Pages/Category.jsx';
 import Product from './Pages/Product.jsx';
+import clothingbanner from './assets/clothingbanner.png'
+import electronicsbanner from './assets/electronicsbanner.png'
+import cosmeticsbanner from './assets/cosmeticsbanner.png'
+
 export default function App() {
   return (
     <BrowserRouter>
     <Header/>
     <Routes>
       <Route path="/" element={<Home/>}/>
-      <Route path="/clothing" element={<Category/>}/>
-      <Route path="/cosmetics" element={<Category/>}/>
-      <Route path="/electronics" element={<Category/>}/>
+      <Route path="/clothing" element={<Category category={"clothing"} banner={clothingbanner}/>}/>
+      <Route path="/cosmetics" element={<Category category={"cosmetics"} banner={cosmeticsbanner}/>}/>
+      <Route path="/electronics" element={<Category category={"electronics"} banner={electronicsbanner}/>}/>
       <Route path="/product" element={<Product/>}/>
       <Route path=":productId" element={<Product/>}/>
     </Routes>
--- client/src/Pages/Category.jsx
+++ client/src/Pages/Category.jsx
@@ -1,8 +1,35 @@
 import React from 'react'
+import { VscSettings} from 'react-icons/vsc'
+import all_products from '../assets/all_products.js'
+import Item from '../components/Item.jsx'
+import { Link } from 'react-router-dom'
 
-const Category = () => {
+const Category = ({banner , category}) => {
   return (
-    <div>Category</div>
+    <section className='bg-primary'>
+      <div className="max-padd-container pt-6">
+        <div className="">
+          <img src={banner} alt="" className=' block mb-7 mx-auto' />
+        </div>
+        <div className="flexBetween my-10 mx-2">
+          <h5><span className='font-bold'>Showing 1-12</span> out of 36 products</h5>
+          <Link to={'/'}
+          className='text-3xl bg-tertiary rounded-md h-10 w-10 p-2 text-white'><VscSettings/></Link>
+        </div>
+        <div className="grid grid-cols-1 xs:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-6 gap-y-28 mt-32">
+          {all_products.map((item)=>{
+            if(category === item.category){
+              return <Item key={item.id} id={item.id} name={item.name} 
+              image={item.image} new_price={item.new_price} 
+              old_price={item.old_price}/>
+            }
+          })}
+        </div>
+        <div className="py-16 text-center">
+          <button className='btn-dark rounded-xl'>Load More</button>
+        </div>
+      </div>
+    </section> 
   )
 }
 

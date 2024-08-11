c7947951817b6481fe78fc38caac090bf11fa7b4
3MaheshKadam <maheshkadam9298@gmail.com>
2024-08-11 09:29:30 +0530

    mobile view

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,68 +1,92 @@
-import React, { useEffect ,useState } from 'react'
-import {Link, NavLink} from 'react-router-dom'
-import Navbar from './Navbar'
+import React, { useEffect, useState } from "react";
+import { Link, NavLink } from "react-router-dom";
+import Navbar from "./Navbar";
 
-import logo from '../assets/logo.png'
-import user from '../assets/user.svg'
-import {MdClose, MdMenu} from 'react-icons/md'
-import {RiShoppingCart2Line} from 'react-icons/ri'
+import logo from "../assets/logo.png";
+import user from "../assets/user.svg";
+import { MdClose, MdMenu } from "react-icons/md";
+import { RiShoppingCart2Line } from "react-icons/ri";
 
 const Header = () => {
-  const[menuOpened ,setMenuOpened]=useState(false); 
-  const toggleMenu =()=> setMenuOpened(!menuOpened);
-  useEffect(()=>{
-    const handleScroll=()=>{
-    if(window.scrollY > 0){
-      //close the menu if opened
-      if(menuOpened){
-        setMenuOpened(false);
+  const [menuOpened, setMenuOpened] = useState(false);
+  const toggleMenu = () => setMenuOpened(!menuOpened);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 0) {
+        // Close the menu if opened
+        if (menuOpened) {
+          setMenuOpened(false);
+        }
       }
     };
-  };
-    window.addEventListener("scroll" , handleScroll);
-    return()=>{
-      window.removeEventListener("scroll",handleScroll);
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
     };
-
-  },[menuOpened]);
+  }, [menuOpened]);
 
   return (
-    <header className='p-2 w-full z-50  max-padd-container '>
-      <div className="flexBetween py-4 ">
-        <Link to={'/'} className='flex items-center gap-x-2'>
-        <img src={logo} alt="logoImg" height={31} width={31} />
-        <span className='bold-28 hidden xs:flex'>Kadam</span>
+    <header className="p-2 w-full z-50 max-padd-container">
+      <div className="flexBetween py-4">
+        <Link to={"/"} className="flex items-center gap-x-2">
+          <img src={logo} alt="logoImg" height={31} width={31} />
+          <span className="bold-28 hidden xs:flex">Kadam</span>
         </Link>
-        <div className="flexCenter mx-20 gap-x-4 "> 
+        <div className="flexCenter mx-20 gap-x-4">
           {/* pc */}
           <div className="mx-10">
-          <Navbar containerStyles={"hidden xl:flex gap-x-5 xl:gap-x-10 medium-25 rounded-full px-2 py-1"}/>
-          </div> 
-          {/* mobile */}
-          <div className=""> 
-          <Navbar containerStyles={`${menuOpened ? "flex item-start flex-col gap-y-12 fixed top-20 right-8 p-12 bg-white rounded-3xl shadow-md w-64 medium-16 ring-1 ring-slate-900/5 transition-all duration-300 z-50" : 
-            "flex item-start flex-col gap-y-12 fixed top-20 p-12 bg-white rounded-3xl shadow-md w-64 medium-16 ring-1 ring-slate-900/5 transition-all duration-300 z-50 -right-[100%]"}`}/>
-          </div> 
-          <div className="flexBetween gap-x-3 sm:gap-x-2 bold-16">
-          {!menuOpened ? (
-            <MdMenu className='xl:hidden cursor-pointer text-3xl hover:text-secondary'onClick={toggleMenu}/>
-          ):(<MdClose className='xl:hidden cursor-pointer text-3xl hover:text-secondary'onClick={toggleMenu}/>)}
-
-          <div className="flexBetween sm:gap-x-6 ">
-            <NavLink to={'/'} className={'flex'}>
-            <RiShoppingCart2Line className='p-2 h-10 w-10'/>
-            <span className='relative flexCenter w-5 h-5 rounded-full bg-secondary text-primary font-medium-14 -top-2 right-3'>{0}</span>
-            </NavLink>
-            <NavLink to={'/'} className={'btn-secondary flexCenter gap-x-2 medium-16 rounded-xl'}>
-            <img src={user} alt="" height={19} width={19}/>
-            Login
-            </NavLink>
+            <Navbar
+              containerStyles={
+                "hidden xl:flex gap-x-5 xl:gap-x-10 medium-25 rounded-full px-2 py-1"
+              }
+            />
           </div>
+          {/* mobile */}
+          <div>
+            <Navbar
+              containerStyles={`${
+                menuOpened
+                  ? "flex item-start flex-col gap-y-12 fixed top-20 right-8 p-12 bg-white rounded-3xl shadow-md w-64 medium-16 ring-1 ring-slate-900/5 transition-all duration-300 z-50"
+                  : "flex item-start flex-col gap-y-12 fixed top-20 p-12 bg-white rounded-3xl shadow-md w-64 medium-16 ring-1 ring-slate-900/5 transition-all duration-300 z-50 -right-[100%]"
+              }`}
+            />
+          </div>
+          <div className="flexBetween gap-x-3 sm:gap-x-2 bold-16">
+            {!menuOpened ? (
+              <MdMenu
+                className="xl:hidden cursor-pointer text-3xl hover:text-secondary"
+                onClick={toggleMenu}
+              />
+            ) : (
+              <MdClose
+                className="xl:hidden cursor-pointer text-3xl hover:text-secondary"
+                onClick={toggleMenu}
+              />
+            )}
+
+            <div className="flexBetween sm:gap-x-6">
+              <NavLink to={"/"} className={"flex"}>
+                <RiShoppingCart2Line className="p-2 h-10 w-10" />
+                <span className="relative flexCenter w-5 h-5 rounded-full bg-secondary text-primary font-medium-14 -top-2 right-3">
+                  {0}
+                </span>
+              </NavLink>
+              {/* <NavLink
+                to={"/"}
+                className={
+                  "hidden sm:flex btn-secondary flexCenter gap-x-2 medium-16 rounded-xl"
+                }
+              >
+                <img src={user} alt="" height={19} width={19} />
+                Login
+              </NavLink> */}
+            </div>
           </div>
         </div>
       </div>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
--- client/src/components/Item.jsx
+++ client/src/components/Item.jsx
@@ -1,28 +1,36 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import {RiShoppingCart2Line} from 'react-icons/ri'
+import React from "react";
+import { Link } from "react-router-dom";
+import { RiShoppingCart2Line } from "react-icons/ri";
+
 const Item = ({ id, name, image, old_price, new_price }) => {
   return (
-    <Link onClick={window.scrollTo(0,0)} to={`/product/${id}`}className='bg-white p-2 rounded-xl relative'>
-      <div className='flexCenter'>
-        <img src={image} alt={name} height={211} width={211} className='rounded-lg drop-shadow-xl absolute bottom-40 '/>
+    <Link
+      onClick={window.scrollTo(0, 0)}
+      to={`/product/${id}`}
+      className="bg-white p-4 rounded-xl relative"
+    >
+      <div className="flex justify-center items-center">
+        <img
+          src={image}
+          alt={name}
+          height={211}
+          width={211}
+          className="rounded-lg drop-shadow-xl mb-4"
+        />
       </div>
-      <div className="flex flex-col gap-y-3 pt-24">
-        <h4 className='line-clamp-2 medium-16'>{name}</h4>
-        {/* <p>Old Price: ${old_price}</p>
-        <p>New Price: ${new_price}</p> */}
+      <div className="flex flex-col gap-y-3 pt-5">
+        <h4 className="line-clamp-2 medium-16">{name}</h4>
         <p>Lorem, Nobis eaque a eum vitae! Vero odio nemo ad minima.</p>
-      <div className="flexBetween">
-        <div className="flex gap-x-4 medium-16">
-            <span >${new_price}.00</span>
-            <span className='line-through text-secondary'>${old_price}.00</span>
+        <div className="flex justify-between">
+          <div className="flex gap-x-4 medium-16">
+            <span>${new_price}.00</span>
+            <span className="line-through text-secondary">${old_price}.00</span>
+          </div>
+          <RiShoppingCart2Line className="p-2 h-10 w-10 hover:text-secondary" />
         </div>
-        <RiShoppingCart2Line className='p-2 h-10 w-10 hover:text-secondary'/>
       </div>
-      </div>
-
     </Link>
-  )
-}
+  );
+};
 
-export default Item
+export default Item;
